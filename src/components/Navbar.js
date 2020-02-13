2cb7c38dee89f0133a7756965761f6c9e2d740a3
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2020-02-13 12:14:42 +0100

    quotecard component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="Navbar">
-        <a href="https://fr.wikipedia.org/wiki/Les_Simpson">La famille Simpson</a>
+        <a href="https://fr.wikipedia.org/wiki/Les_Simpson">Simpsons Quotes</a>
     </nav>
   );
 }
