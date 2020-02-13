8f92b999a9386f62465f954816de1cb94e0b157f
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2020-02-13 11:55:52 +0100

    ajout css

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+        <a href="https://fr.wikipedia.org/wiki/Les_Simpson">La famille Simpson</a>
+    </nav>
+  );
+}
+
+export default Navbar;
